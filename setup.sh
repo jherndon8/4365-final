fd7f7ac0ea2d9beec43ed388f1a2e0086efc9962
Jonathan Boutsicaris <cptarnie@github.com>
2018-02-28 02:36:35 -0500

    setup script now includes step 2

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-# Emulab project setup script
+# Emulab roject setup script
 # Run this script on Emulab Ops Server
 
 # Custom inputs
 USER=boots
 PROJECT=Infosphere
+EXPERIMENT=cloudsurfing
 
 # Useful variables
 ELBA_REPO=https://github.com/coc-gatech-newelba/NewElbaAlpha.git
@@ -29,3 +30,48 @@ mkdir -p /proj/Infosphere/$USER/softwares/data
 cp -r ./NewElbaAlpha/mScopeEventMonitors /proj/Infosphere/$USER/softwares
 cp -r ./NewElbaAlpha/mScopeResourceMonitors /proj/Infosphere/$USER/softwares
 cp /proj/Infosphere/shared_software/rubbos_orig_data.tar.gz /proj/Infosphere/$USER/softwares/data
+
+# Describing the Experiment by editing/completing Experiment XML
+cd /proj/Infosphere/$USER/common/experiment_xml
+cp RUBBOS-1111-EMULAB-DEFAULT.xml RUBBOS-1111-EMULAB-$USER.xml
+
+
+# Replace xml values with name and experiment name
+# http://www.yourownlinux.com/2015/05/sed-command-in-linux-search-replace-patterns-in-file.html
+
+sed -i '' "/ <param name=\"EMULAB_EXPERIMENT_NAME\"/s/value=.*/value=\"$EXPERIMENT.infosphere.emulab.net\"\/>/g" /proj/Infosphere/$USER/common/experiment_xml/RUBBOS-1111-EMULAB-boots.xml
+sed -i '' "/ <param name=\"USERNAME\"/s/value=.*/value=\"$USER\"\/>/g" /proj/Infosphere/$USER/common/experiment_xml/RUBBOS-1111-EMULAB-boots.xml
+sed -i '' "/ <param name=\"WORK_HOME\"/s/value=.*/value=\"\/proj\/Infosphere\/$USER\/rubbos\/rubbos_yasu\"\/>/g" /proj/Infosphere/$USER/common/experiment_xml/RUBBOS-1111-EMULAB-boots.xml
+sed -i '' "/ <param name=\"OUTPUT_HOME\"/s/value=.*/value=\"\/proj\/Infosphere\/$USER\/rubbos\/rubbos_yasu\/$EXPERIMENT\"\/>/g" /proj/Infosphere/$USER/common/experiment_xml/RUBBOS-1111-EMULAB-boots.xml
+sed -i '' "/ <param name=\"RUBBOS_RESULTS_HOST\"/s/value=.*/value=\"$USER@users.emulab.net\"\/>/g" /proj/Infosphere/$USER/common/experiment_xml/RUBBOS-1111-EMULAB-boots.xml
+sed -i '' "/ <param name=\"RUBBOS_RESULTS_DIR_BASE\"/s/value=.*/value=\"\/proj\/Infosphere\/$USER\/results\"\/>/g" /proj/Infosphere/$USER/common/experiment_xml/RUBBOS-1111-EMULAB-boots.xml
+sed -i '' "/ <param name=\"RUBBOS_RESULTS_PARSE\"/s/value=.*/value=\"\/proj\/Infosphere\/$USER\/results\"\/>/g" /proj/Infosphere/$USER/common/experiment_xml/RUBBOS-1111-EMULAB-boots.xml
+
+sed -i '' "/ <param name=\"psMonitor\"/s/value=.*/value=\"false\"\/>/g" /proj/Infosphere/$USER/common/experiment_xml/RUBBOS-1111-EMULAB-boots.xml
+sed -i '' "/ <param name=\"iostatMonitor\"/s/value=.*/value=\"false\"\/>/g" /proj/Infosphere/$USER/common/experiment_xml/RUBBOS-1111-EMULAB-boots.xml
+sed -i '' "/ <param name=\"sarMonitor\"/s/value=.*/value=\"false\"\/>/g" /proj/Infosphere/$USER/common/experiment_xml/RUBBOS-1111-EMULAB-boots.xml
+sed -i '' "/ <param name=\"XML_ENABLED\"/s/value=.*/value=\"false\"\/>/g" /proj/Infosphere/$USER/common/experiment_xml/RUBBOS-1111-EMULAB-boots.xml
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
